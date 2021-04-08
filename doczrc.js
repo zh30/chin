7b72300f768c9e9d14bd314f9d0f19cb1ea41f18
He Zhang <zhanghecool@outlook.com>
2021-04-08 14:20:47 +0800

    feat(init): 初始化项目目录

--- doczrc.js
+++ doczrc.js
@@ -6,6 +6,6 @@ export default {
   dest: "docsite", // 打包 docz 文档到哪个文件夹下
   title: "Chin UI", // 设置文档的标题
   typescript: true, // 支持 typescript 语法
-  themesDir: "theme", // 主题样式放在哪个文件夹下，后面会讲
-  menu: ["快速上手", "业务组件"] // 生成文档的左侧菜单分类
+  // themesDir: "theme", // 主题样式放在哪个文件夹下，后面会讲
+  menu: ["GettingStarted", "Components"] // 生成文档的左侧菜单分类
 };
